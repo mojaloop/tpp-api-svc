fe03cca60bfc1512d278e18cdb5690a26eddfbec
Artificed <paultsai.id@gmail.com>
2026-05-28 19:56:59 +0700

    fix(dfsp-simulator): return 202 from POST /tppAccountsRequests
    Signed-off-by: Artificed <paultsai.id@gmail.com>

--- src/dfsp-simulator/handlers/accounts.ts
+++ src/dfsp-simulator/handlers/accounts.ts
@@ -1,9 +1,5 @@
 import { Request, ResponseToolkit } from '@hapi/hapi'
 
-export async function postTppAccountsRequest(request: Request, h: ResponseToolkit) {
-  const id = request.params.id
-
-  return h.response({
-    status: 'ACCEPTED'
-  }).code(200)
+export async function postTppAccountsRequest(_request: Request, h: ResponseToolkit) {
+  return h.response().code(202)
 }
